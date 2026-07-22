31b1f2c329f5bbd86a6138832283a01bf02f53c3
allenyllee <allen7575@gmail.com>
2026-07-22 22:56:37 +0800

    Disable notifications in Brave regression tests

--- scripts/brave-regression.test.cjs
+++ scripts/brave-regression.test.cjs
@@ -183,6 +183,7 @@ async function run() {
     `--remote-debugging-port=${port}`,
     '--remote-allow-origins=*',
     '--headless=new',
+    '--disable-notifications',
     '--no-first-run',
     '--no-default-browser-check',
     'about:blank',
